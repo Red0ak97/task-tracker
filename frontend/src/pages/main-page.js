5db44084ad38087b84372ba359cb4a468116ee4d
Red0ak97~ <dk08082017@gmail.com>
2025-08-31 22:31:16 +0300

    use webpack bundler

---
+++ frontend/src/pages/main-page.js
@@ -0,0 +1,19 @@
+import { TasksPage } from "./tasks-page.js";
+import { UsersPage } from "./users-page.js";
+import { Router } from "../core/index.js";
+import { NotFoundPage } from "./not-found-page.js";
+import { NavBar } from "../components/index.js";
+
+import '../styles/main.css';
+
+// render navigation for all the pages
+new NavBar({ container: document.body }).render();
+
+const container = document.querySelector("#app");
+const routerConfig = {
+  "/": () => new TasksPage({ container }).render(),
+  "/users": () => new UsersPage({ container }).render(),
+  notFound: () => new NotFoundPage({ container }).render(),
+};
+
+new Router(routerConfig);
